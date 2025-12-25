8d50040ffd43cde78c541c4c2570f7975f8307b4
AdamWong <408579011@qq.com>
2025-12-25 16:59:48 +0800

    add 增加测试文件

---
+++ test/conformance/conformance_test.go
@@ -0,0 +1,23 @@
+package conformance
+
+import (
+	"testing"
+
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+
+	_ "github.com/dspo/go-homework/conformance"
+	"github.com/dspo/go-homework/sdk"
+	"github.com/dspo/go-homework/test/framework"
+)
+
+func TestConformance(t *testing.T) {
+	// do your init jobs, e.g. deploy service, database, prepare data
+
+	var _ = sdk.NewSDK("127.0.0.1:8086") // init sdk with your app server address
+
+	gomega.RegisterFailHandler(ginkgo.Fail)
+	framework.GetFramework(t).DeployComponents()
+
+	ginkgo.RunSpecs(t, "Conformance Suite")
+}
