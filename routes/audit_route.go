30f56e534d297f97c89d432ced9509b7206c1916
AdamWong <408579011@qq.com>
2025-12-28 22:20:04 +0800

    debug 审计增加鉴权

--- routes/audit_route.go
+++ routes/audit_route.go
@@ -4,11 +4,14 @@ import (
 	"project-manager/controllers"
 
 	"github.com/gin-gonic/gin"
+
+	"project-manager/middleware"
 )
 
 func InitAuditRoutes(r *gin.Engine) {
 	auditGroup := r.Group("/api/audits")
 	{
-		auditGroup.GET("", controllers.Audit.List)
+		//使用鉴权中间件
+		auditGroup.GET("", controllers.Audit.List).Use(middleware.AuthRequired())
 	}
 }
